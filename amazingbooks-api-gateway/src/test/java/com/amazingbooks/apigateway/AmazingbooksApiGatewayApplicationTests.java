69c4b5408d5f6060ca48b75c5a98bed33b609099
sumitkumar1094 <49199223+sumitkumar1094@users.noreply.github.com>
2022-04-25 13:22:27 +0530

    Add files via upload

---
+++ amazingbooks-api-gateway/src/test/java/com/amazingbooks/apigateway/AmazingbooksApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazingbooks.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingbooksApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
